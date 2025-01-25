b574d8d88fce77092c9af806f5fadd69f5cbcdb5
brein594 <brein594@gmail.com>
2025-01-25 20:16:30 +0300

    fix: updat dependencies in kts and path SomeClassTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,8 @@ repositories {
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher") {
-        because("Only needed to run tests in a version of IntelliJ IDEA that bundles older versions")
-    }
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
